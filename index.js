5c527e10fb1dbe1d9b6dcb2160ad35c3130962fa
F-K-C <fakocarvalho@gmail.com>
2024-05-13 17:33:03 +0000

    Endpoint ReadByID criado

--- index.js
+++ index.js
@@ -12,5 +12,12 @@ app.get('/personagem', function(req, res){
     res.send(lista)
 })
 
+//EndPoint Read By ID
+app.get('/personagem/:id', function(req, res) {
+    const id = req.params.id
+    const item = lista[id - 1]
+
+    res.send(item)
+})
 
 app.listen(3000)
