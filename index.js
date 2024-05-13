4b15eb7a7cde76b33bf8ff23dd355d75c49a6606
F-K-C <fakocarvalho@gmail.com>
2024-05-13 17:22:46 +0000

    Endpoint ReadlAll

--- index.js
+++ index.js
@@ -5,4 +5,12 @@ app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
+const lista = ['Java', 'Kotlin', 'Android']
+
+// Endpoint Readl all (GET) /personagem
+app.get('/personagem', function(req, res){
+    res.send(lista)
+})
+
+
 app.listen(3000)
